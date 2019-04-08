4dca94d46936207ac1404ee315e6cc212557fa87
王路 <lu.wang@emaxcard.com>
2019-04-08 17:36:33 +0800

    优化代码

--- src/main/java/com/ims/shiro/realm/UserRealm.java
+++ src/main/java/com/ims/shiro/realm/UserRealm.java
@@ -11,6 +11,24 @@ import org.apache.shiro.util.ByteSource;
 
 import javax.annotation.Resource;
 
+/***
+ * 
+* 
+* 描    述：
+*
+* 创 建 者： @author wl
+* 创建时间： 2019/4/8 14:22
+* 创建描述：
+* 
+* 修 改 者：  
+* 修改时间： 
+* 修改描述： 
+* 
+* 审 核 者：
+* 审核时间：
+* 审核描述：
+*
+ */
 public class UserRealm extends AuthorizingRealm {
 
     @Resource
@@ -29,19 +47,19 @@ public class UserRealm extends AuthorizingRealm {
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
         String username = (String) token.getPrincipal();
         User user = userService.getUserByUsername(username);
 
         if (user == null) {
             throw new UnknownAccountException();
         }
-
-//        如果帐号锁定，输出
+        // 如果帐号锁定，输出
         if(user.getState()==2){
             throw new DisabledAccountException();
         }
 
-//        如果帐号未激活，输出
+        //  如果帐号未激活，输出
         if(user.getState()==0){
             throw new LockedAccountException();
         }
